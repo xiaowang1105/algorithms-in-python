106691944a4a4a4a9cf00c768f0d464c890c6443
Xiao Wang <wx961105@outlook.com>
2018-03-18 13:54:13 +0800

    Add files via upload

---
+++ 4.0 Greedy Algorithms/shedule_activities.py
@@ -0,0 +1,47 @@
+class Job:
+    def __init__(self, weight, length):
+        self.weight = weight
+        self.length = length
+    @classmethod
+    def read_from_dict(cls, job_dict):
+        job_list = []
+        for item in job_dict.values():
+            job_list.append(cls(item[0], item[1]))
+        return job_list
+    @staticmethod
+    def read_file(path='jobs.txt'):
+        with open(path) as f:
+            num_jobs = int(f.readline().strip('\n'))
+            jobs = {}
+            for index, item in enumerate(f.readlines()):
+                item = item.strip('\n').split(' ')
+                jobs[index] = list(map(int, item))
+        return (num_jobs, jobs)
+    @staticmethod
+    def sort(job_list, method):
+        if method == 'minus':
+            return sorted(job_list, key=lambda job: \
+                (job.weight - job.length, job.weight), reverse=True)
+        elif method == 'divide':
+            return sorted(job_list, key=lambda job: \
+                (job.weight / job.length, job.weight), reverse=True)
+        else:
+            raise SyntaxError('Input Sort Method Wrong')
+    @staticmethod
+    def completion_time(job_list):
+        completion_time = 0
+        current_time = 0
+        for job in job_list:
+            current_time += job.length
+            completion_time += current_time * job.weight
+        return completion_time
+
+def main():
+    job_list = Job.read_from_dict(Job.read_file()[1])
+    job_list = Job.sort(job_list, 'minus')
+    print('greedy method is minus completion time: ', Job.completion_time(job_list))
+    job_list = Job.sort(job_list, 'divide')
+    print('greedy method is divide completion time: ', Job.completion_time(job_list))
+
+if __name__ == '__main__':
+    main()
---
+++ 4.0 Greedy Algorithms/union_find.py
@@ -0,0 +1,38 @@
+class Union_Find:
+    """class for union find data structure
+
+    >>> a, b, c = Union_Find(), Union_Find(), Union_Find()
+    >>> [a.rank, b.rank, c.rank]
+    [0, 0, 0]
+    >>> c == Union_Find.union(c, a)
+    True
+    >>> b == Union_Find.union(a, b)
+    True
+    >>> [a.rank, b.rank, c.rank]
+    [1, 0, 0]
+    """
+    def __init__(self):
+        self.parent = self
+        self.rank = 0
+    def find_set(self):
+    #TODO alternative solution
+        if self.parent != self:
+            self.parent = self.parent.find_set()
+        return self.parent
+    @staticmethod
+    def link(x, y):
+        if x.rank > y.rank:
+            delete = y.parent
+            y.parent = x
+        else:
+            delete = x.parent
+            x.parent = y
+            y.rank = y.rank + 1 if x.rank == y.rank else y.rank
+        return delete
+    @classmethod
+    def union(cls, x, y):
+        return cls.link(x.find_set(), y.find_set())
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
